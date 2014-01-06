c0a43741d0d3146d1749f8402a09e66d97cee8af
michiroooo <h.michiro@gmail.com>
2014-01-07 00:03:01 +0900

    add function chain example with variadic template

---
+++ mpp/src/chain.h
@@ -0,0 +1,47 @@
+/*
+ *  mpp.h
+ *  mpp
+ *
+ *  Created by Michiro Hirai on 1/6/14.
+ *  Copyright (c) 2014 michiroooo. All rights reserved.
+ *
+ */
+
+#ifndef mpp_
+#define mpp_
+
+#pragma GCC visibility push(default)
+
+#include <iostream>
+#include <functional>
+
+namespace mpp { namespace func {
+
+//    template<typename R>
+//    auto chain(std::function<R()> func, std::function<R()> next) -> decltype(func() + next())
+//    {
+//        if (next)
+//            return func() + next();
+//        return func();
+//    };
+//
+//    template<typename R, typename Arg>
+//    R chain(std::function<R(Arg)> func, std::function<R(Arg)> next, Arg arg)
+//    {
+//        if (next)
+//            return func(arg) + next(arg);
+//        return func(arg);
+//    };
+
+    template<typename R, typename ...Args>
+    auto chain(std::function<R(Args...)> func, std::function<R(Args...)> next, Args ... args) -> decltype(func(args...) + next(args...))
+    {
+        if (next) return func(args...) + next(args...);
+        return func(args...);
+    }
+
+} }
+
+
+#pragma GCC visibility pop
+#endif
---
+++ mpp/test/test.cpp
@@ -0,0 +1,64 @@
+//=======================================================================================
+/*!
+ * \file test.cpp
+ *
+ * \defgroup mpp
+ *
+ * \brief 
+ * Created by Michiro Hirai on 1/6/14.
+ *
+ */
+//   Copyright (c) 2014 michiroooo. All rights reserved.:
+//    (C) Copyright 2013 Sony. All rights reserved. Use of copyright notice is
+//        precautionary and does imply publication.
+//=======================================================================================
+
+
+#include <memory>
+#include "chain.h"
+
+class math {
+
+public:
+    float ten(){ return 10.0f; }
+    float squrare(float rhs){
+        return rhs * rhs;
+    }
+    float prod(float rhs, float lhs){ return rhs * lhs; }
+};
+
+int main(int argc, char ** argv)
+{
+    using std::placeholders::_1;
+    using std::placeholders::_2;
+    using mpp::func::chain;
+
+    std::function<float()> cf;
+    std::function<float()> ten = std::bind(&math::ten, math());
+    cf = std::bind(&chain<float>, ten, cf);
+
+    auto result = cf(); // execute
+    std::cerr << result << std::endl;
+
+    std::function<float(float)> chained;
+    std::function<float(float)> square = std::bind(&math::squrare, math(), _1);
+
+    chained = std::bind(&chain<float, float>, square, chained, _1);
+    chained = std::bind(&chain<float, float>, square, chained, _1);
+
+    result = chained(0.1); // execute
+    std::cerr << result << std::endl;
+
+
+    std::function<float(float, float)> chained_func;
+    std::function<float(float, float)> prod = std::bind(&math::prod, math(), _1, _2);
+
+    chained_func = std::bind(&chain<float, float, float>, prod, chained_func, _1, _2);
+    chained_func = std::bind(&chain<float, float, float>, prod, chained_func, _1, _2);
+
+    result = chained_func(0.1, 0.2); // execute
+
+    std::cerr << result << std::endl;
+
+    return 0;
+}
